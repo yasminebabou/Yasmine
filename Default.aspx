bf8f11dd4116d9f14c2529623026c75041796a9a
yasminebabou <49848690+yasminebabou@users.noreply.github.com>
2019-04-22 09:02:31 +0800

    Add files via upload

---
+++ Default.aspx
@@ -0,0 +1,66 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="test._Default" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+
+    <div class="jumbotron">
+        <h1><strong>Motor Winding Temperature Calculation</strong></h1>
+        <p class="lead">Input your data and then Run </p>
+    </div>
+
+    <div class="row">
+        <div class="col-md-4">
+            <h2>Input Data </h2>
+            <p>Motor Series :&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                <asp:ListBox ID="ListBox1" runat="server" Height="18px" Width="79px" required="yes">
+                    <asp:ListItem Value="350"></asp:ListItem>
+                    <asp:ListItem Value="380"></asp:ListItem>
+                    <asp:ListItem Value="450"></asp:ListItem>
+                    <asp:ListItem Value="450E"></asp:ListItem>
+                    <asp:ListItem Value="550"></asp:ListItem>
+                    <asp:ListItem>650</asp:ListItem>
+                    <asp:ListItem>740</asp:ListItem>
+                </asp:ListBox>
+            </p>
+            <p>Motor HP : <asp:TextBox ID="TextBox1" runat="server" Height="16px" Width="74px" required="yes"></asp:TextBox>
+&nbsp;HP</p>
+            <p>Pump HP :
+                <asp:TextBox ID="TextBox2" runat="server" Height="16px" Width="74px" required="yes"></asp:TextBox>
+&nbsp;HP</p>
+            <p>Bottom Hole Temperature :
+                <asp:TextBox ID="TextBox3" runat="server" Height="16px" Width="74px" required="yes"></asp:TextBox>
+&nbsp;°F</p>
+            <p>Pumping Flow Rate :
+                <asp:TextBox ID="TextBox4" runat="server" Height="16px" Width="74px" required="yes" ></asp:TextBox>
+&nbsp;</p>
+            <p>Well Casing ID :
+                <asp:TextBox ID="TextBox5" runat="server" Height="16px" Width="74px" required="yes"></asp:TextBox>
+&nbsp;Inch</p>
+            <p>Water cut :
+                <asp:TextBox ID="TextBox6" runat="server" Height="16px" Width="74px" required="yes"></asp:TextBox>
+&nbsp;%</p>
+            <p>API :
+                <asp:TextBox ID="TextBox7" runat="server" Height="16px" Width="74px" required="yes"></asp:TextBox>
+&nbsp;</p>
+            <p>Pump Intake Pressure PSI :
+                <asp:TextBox ID="TextBox8" runat="server" Height="16px" Width="74px" required="yes"></asp:TextBox>
+&nbsp;Psi</p>
+            <p>Bubble Point PSI :
+                <asp:TextBox ID="TextBox9" runat="server" Height="16px" Width="74px" required="yes"></asp:TextBox>
+&nbsp;Psi</p>
+            <p>Solution Gas-Oil-Ratio at the intake SCF/STB :
+                <asp:TextBox ID="TextBox10" runat="server" Height="16px" Width="74px" required="yes"></asp:TextBox>
+            </p>
+            <p>Surface Gas-Oil-Ratio SCF/STB :&nbsp;
+                <asp:TextBox ID="TextBox11" runat="server" Height="16px" Width="74px" required="yes"></asp:TextBox>
+            </p>
+            <p>Motor Running Frequency :
+                <asp:TextBox ID="TextBox12" runat="server" Height="16px" Width="74px" required="yes"></asp:TextBox>
+&nbsp; Hz</p>
+        
+            <asp:Button ID="button_Click" runat="server" type="submit" Height="16px" Text="Run" Width="40px" OnClick="button_Click_Click" />
+            
+            &nbsp;</p>
+        </div>
+    </div>
+
+</asp:Content>
---
+++ Default.aspx.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.IO;
+using System.Text;
+using System.Collections.Generic; 
+using System.ComponentModel; 
+using System.Data; 
+using System.Drawing; 
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace test
+{
+    public partial class _Default : Page
+    {
+        
+
+
+        protected void button_Click_Click(object sender, EventArgs e)
+        {
+            FileStream fs = File.Create(@"C:\Users\Leo\Documents\fichier.txt");
+
+            //File.WriteAllText(@"C:\Users\Leo\Documents\mtc\test\winp.txt", ListBox1.Text +  "    "+"Motor Series " + Environment.NewLine + TextBox1.Text + "    "+"Motor HP " + Environment.NewLine +  TextBox2.Text + "    " + "Pump HP " + Environment.NewLine +  TextBox3.Text + "    " + "Bottom Hole Temperature " + Environment.NewLine +  TextBox4.Text + "    " + "Pumping Flow Rate " + Environment.NewLine +  TextBox5.Text + "    " + "Well Casing ID  " + Environment.NewLine +  TextBox6.Text + "    " + "Water cut" + Environment.NewLine +  TextBox7.Text+ "    " + "API " + Environment.NewLine +  TextBox8.Text + "    " + "Pump Intake Pressure PSI  " + Environment.NewLine +  TextBox9.Text + "    " + "Bubble Point PSI " + Environment.NewLine +  TextBox10.Text + "    " + "Solution Gas-Oil-Ratio at the intake SCF/STB " + Environment.NewLine +  TextBox11.Text + "    " + "SurfaceGas-Oil-RatioSCF/STB" + Environment.NewLine +  TextBox12.Text + "    " + "Motor Running Frequency", Encoding.Unicode);
+            File.WriteAllText(@"C:\Users\Leo\Documents\mtc\test\winp.txt", ListBox1.Text + "    " + "Motor Series " + Environment.NewLine + TextBox1.Text + "    " + "Motor HP " + Environment.NewLine + TextBox2.Text + "    " + "Pump HP " + Environment.NewLine + TextBox3.Text + "    " + "Bottom Hole Temperature " + Environment.NewLine + TextBox4.Text + "    " + "Pumping Flow Rate " + Environment.NewLine + TextBox5.Text + "    " + "Well Casing ID  " + Environment.NewLine + TextBox6.Text + "    " + "Water cut" + Environment.NewLine + TextBox7.Text + "    " + "API " + Environment.NewLine + TextBox8.Text + "    " + "Pump Intake Pressure PSI  " + Environment.NewLine + TextBox9.Text + "    " + "Bubble Point PSI " + Environment.NewLine + TextBox10.Text + "    " + "Solution Gas-Oil-Ratio at the intake SCF/STB " + Environment.NewLine + TextBox11.Text + "    " + "SurfaceGas-Oil-RatioSCF/STB" + Environment.NewLine + TextBox12.Text + "    " + "Motor Running Frequency", Encoding.Unicode);
+
+            fs.Close();
+            // Process p = new Process();
+
+            // p.WaitForInputIdle(@"C:\Users\Leo\Documents\winp.txt");
+            //p.OutputDataReceived(@"C:\Users\Leo\Documents\wout.txt")
+            //Process.Start(@"C:\Users\Leo\Documents\mtc\HRAPC4.exe", @"C: \Users\Leo\Documents\winp.txt");
+
+
+
+            //ProcessStartInfo startInfo = new ProcessStartInfo(@"C:\Users\Leo\Documents\mtc\HRAPC4.exe");
+           // startInfo.WindowStyle = ProcessWindowStyle.Normal;
+           // startInfo.Arguments = (@"C: \Users\Leo\Documents\winp.txt"); 
+            //Process.Start(startInfo);
+
+
+            // 
+            ProcessStartInfo psi = new ProcessStartInfo();
+
+            psi.FileName = @"C:\Users\Leo\Documents\mtc\test\HRAPC4.exe";
+            //psi.FileName = "Notepad++.exe";
+            psi.Arguments = @"C:\Users\Leo\Documents\mtc\test\winp.txt";
+
+            Process p = Process.Start(psi);
+            p.WaitForExit(5000000);
+
+        }
+
+    }
+}
